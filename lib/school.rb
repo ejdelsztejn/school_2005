f90bfcc2362ab2ea1fe089e54d044cbbad4a9e28
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-05-06 15:11:52 -0400

    :sparkles: Add attr_readers and student_names
    Add attr_readers and student_names

--- lib/school.rb
+++ lib/school.rb
@@ -1,3 +1,12 @@
 class School
+  attr_reader :start_time,
+              :hours_in_school_day,
+              :student_names
+
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
 
 end
